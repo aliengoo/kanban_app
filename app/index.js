70727933b39ab5437e62f875c49a5d49f7bf3986
Alien Goo <glenn.android@gmail.com>
2015-11-01 20:03:36 +0000

    Initial commit

---
+++ app/index.js
@@ -0,0 +1,10 @@
+"use strict";
+require("./main.scss");
+
+var component = require('./component');
+
+var app = document.createElement('div');
+
+document.body.appendChild(app);
+
+app.appendChild(component());
---
+++ webpack.config.js
@@ -0,0 +1,61 @@
+var webpack = require('webpack');
+var path = require('path');
+var HtmlwebpackPlugin = require('html-webpack-plugin');
+var merge = require('webpack-merge');
+
+var TARGET = process.env.npm_lifecycle_event;
+var ROOT_PATH = path.resolve(__dirname);
+var APP_PATH = path.resolve(ROOT_PATH, 'app');
+var BUILD_PATH = path.resolve(ROOT_PATH, 'build');
+console.log(TARGET);
+
+module.exports = {
+  entry: APP_PATH,
+  output: {
+    path: BUILD_PATH,
+    filename: 'bundle.js'
+  },
+  devServer: {
+    historyApiFallback: true,
+    hot: true,
+    inline: true,
+    progress: true
+  },
+  devtool: 'eval-source-map',
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        loaders: ['babel'],
+        include: APP_PATH
+      },
+      // SASS
+      {
+        test: /\.scss$/,
+        loader: 'style!css!sass',
+        include: APP_PATH
+      }
+    ]
+  },
+  plugins: [
+    new HtmlwebpackPlugin({
+      title: 'Kanban app'
+    }),
+    new webpack.HotModuleReplacementPlugin()
+  ]
+};
+//
+//if(TARGET === 'start' || !TARGET) {
+//  module.exports = merge(common, {
+//    devtool: 'eval-source-map',
+//    devServer: {
+//      historyApiFallback: true,
+//      hot: true,
+//      inline: true,
+//      progress: true
+//    },
+//    plugins: [
+//      new webpack.HotModuleReplacementPlugin()
+//    ]
+//  });
+//}
